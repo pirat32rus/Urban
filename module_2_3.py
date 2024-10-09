04e031ee59f48d2866fc3d6f6be62beecda72f80
pirat32rus <pirat32rus@mail.ru>
2024-10-09 21:16:17 +0300

    Update

---
+++ module_2_3.py
@@ -0,0 +1,9 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+number = 0
+
+while number < len(my_list):
+    if my_list[number] < 0:
+        break
+    number += 1
+    if my_list[number - 1] > 0:
+        print(my_list[number - 1])
