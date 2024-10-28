ce4e1ca33082fd5771a690395f62c2e67a0a3527
pirat32rus <pirat32rus@mail.ru>
2024-10-28 15:07:15 +0300

    Update

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as fake_d
+from true_math import divide as true_d
+
+result1 = fake_d(69, 3)
+result2 = fake_d(3, 0)
+result3 = true_d(49, 7)
+result4 = true_d(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
