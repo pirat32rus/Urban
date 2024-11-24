10d3f8b012b512dabd712e882ca456174efd67b6
pirat32rus <pirat32rus@mail.ru>
2024-11-24 11:26:13 +0300

    Update

---
+++ module6hard.py
@@ -0,0 +1,93 @@
+import math
+
+class Figure:
+    def __init__(self, color=(0, 0, 0), *sides):
+        self.__sides = []
+        self.__color = list(color)
+        self.filled = False  # публичный атрибут, указывает, закрашена ли фигура
+
+        self.set_sides(*sides)
+
+    def __is_valid_color(self, r, g, b):
+        return isinstance(r, int) and isinstance(g, int) and isinstance(b, int) and 0 <= r <= 255 and 0 <= g <= 255 and 0 <= b <= 255
+
+    def set_color(self, r, g, b):
+        if self.__is_valid_color(r, g, b):
+            self.__color = [r, g, b]
+
+    def get_color(self):
+        return self.__color
+
+    def __is_valid_sides(self, *new_sides):
+        return all(isinstance(side, int) and side > 0 for side in new_sides) and len(new_sides) == self.sides_count
+
+    def get_sides(self):
+        return self.__sides
+
+    def set_sides(self, *new_sides):
+        if self.__is_valid_sides(*new_sides):
+            self.__sides = list(new_sides)
+
+    def __len__(self):
+        return sum(self.__sides)
+
+class Circle(Figure):
+    sides_count = 1
+
+    def __init__(self, color=(0, 0, 0), *sides):
+        super().__init__(color, *sides)
+        self.__radius = self.get_sides()[0] / (2 * math.pi) if self.get_sides() else 1  # радиус из длины окружности 
+        self.set_sides(1)
+
+    def get_square(self):
+        return math.pi * (self.__radius ** 2)
+
+class Triangle(Figure):
+    sides_count = 3
+
+    def __init__(self, color=(0, 0, 0), *sides):
+        super().__init__(color, *sides)
+        self.set_sides(1, 1, 1)  # начальные стороны, если не переданы другие
+
+    def get_square(self):
+        s = sum(self.get_sides()) / 2
+        a, b, c = self.get_sides()
+        return math.sqrt(s * (s - a) * (s - b) * (s - c))
+
+class Cube(Figure):
+    sides_count = 12
+
+    def __init__(self, color=(0, 0, 0), *sides):
+        super().__init__(color, *sides)
+        if sides:
+            self.set_sides(*([sides[0]] * 12))  # устанавливаем все 12 сторон равными
+        else:
+            self.set_sides(*([1] * 12))  # по умолчанию ставим 1
+
+    def get_volume(self):
+        side = self.get_sides()[0]  # все стороны одинаковы
+        return side ** 3
+
+# Пример проверки:
+circle1 = Circle((200, 200, 100), 10)
+cube1 = Cube((222, 35, 130), 6)
+
+# Проверка на изменение цветов:
+circle1.set_color(55, 66, 77)  # Изменится
+print(circle1.get_color())  # [55, 66, 77]
+
+cube1.set_color(300, 70, 15)  # Не изменится
+print(cube1.get_color())  # [222, 35, 130]
+
+# Проверка на изменение сторон:
+cube1.set_sides(5, 3, 12, 4, 5)  # Не изменится
+print(cube1.get_sides())  # [6, 6, 6, ..., 6]
+
+circle1.set_sides(15)  # Изменится
+print(circle1.get_sides())  # [1]
+
+# Проверка периметра (круга), это и есть длина:
+print(len(circle1))  # 1
+
+# Проверка объёма (куба):
+print(cube1.get_volume())  # 216
